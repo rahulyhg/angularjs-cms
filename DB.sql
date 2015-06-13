a26d3e6432592078452a6e178ff533f163846724
Aran <aran.erlich@gmail.com>
2015-06-13 11:50:38 +0300

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -30,21 +30,6 @@ CREATE TABLE `files` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=43 ;
 
---
--- Dumping data for table `files`
---
-
-INSERT INTO `files` (`id`, `file`, `title`, `alt`, `remove`) VALUES
-(7, '54954a6dd8349_n7F5qX6A_1920x1080.jpg.jpg', 'Flowers', 'wallpaper', 0),
-(26, '54989ceeacb65_My-Nexus4-wallpaper-Patterns.jpeg.jpeg', 'triangle wallpaper', '', 0),
-(28, '549b13872001a_2394-10.jpg.jpg', 'flower', 'alternate text', 0),
-(29, '549b138db454f_7250.jpg.jpg', '', '', 0),
-(30, '549b13946e25a_Clown_Fish_1920x1080HDTV1080p.jpg.jpg', '', '', 0),
-(31, '549b139ee8f82_sea_shore.jpg.jpg', '', '', 0),
-(32, '549b13a9d2ff0_wallpaper-2789936.jpeg.jpeg', 'Sky wallpaper', 'wallpaper', 0),
-(36, '54a9babfdb7a6_elefanto_wallpaper.png.png', '', '', 0),
-(42, '54c54f1e16298_IMG_0414.JPG.jpg', '', '', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -63,15 +48,6 @@ CREATE TABLE `pages` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=26 ;
 
---
--- Dumping data for table `pages`
---
-
-INSERT INTO `pages` (`id`, `title`, `content`, `user_id`, `date`, `lead_image_id`, `template`, `remove`) VALUES
-(18, 'My page', 'My content<br><br><br><img src="files/images/549b13946e25a_Clown_Fish_1920x1080HDTV1080p.jpg.jpg" class="textEreaImage"><br>test<br><img src="files/images/549b13872001a_2394-10.jpg.jpg" class="textEreaImage">', '1', '0000-00-00 00:00:00', '26', 'welcome', 0),
-(21, 'Third page', 'Some content<br><img src="files/images/549b13872001a_2394-10.jpg.jpg" class="textEreaImage">', '1', '2015-04-01 17:38:23', '7', 'aeadmin', 0),
-(22, 'New page', 'Some content here...<br>', '1', '0000-00-00 00:00:00', '28', 'error', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -89,15 +65,6 @@ CREATE TABLE `portfolio` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
---
--- Dumping data for table `portfolio`
---
-
-INSERT INTO `portfolio` (`id`, `title`, `lead_image_id`, `description`, `link`, `category`, `remove`) VALUES
-(1, 'my first portfolio', '29', 'This is about nothing', 'aecreate.it', 'cms', 0),
-(2, 'new one', '30', 'keepass.com', 'keepass.com', 'web app', 0),
-(4, 'Third portfolio', '28', 'Not much...', 'Nowhere', 'None', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -112,13 +79,6 @@ CREATE TABLE `settings` (
   `wallpaper_id` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `settings`
---
-
-INSERT INTO `settings` (`id`, `site_title`, `site_description`, `site_keywords`, `wallpaper_id`) VALUES
-(1, 'Site title', 'Site descriptionsss', 'Site keywords', '42');
 
 -- --------------------------------------------------------
 
